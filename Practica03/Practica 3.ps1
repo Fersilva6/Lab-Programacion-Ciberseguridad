d9df76ac51f39a1c74b04c3bf3d404ebce4e0c76
Fersilva6 <134666529+Fersilva6@users.noreply.github.com>
2023-05-25 22:05:41 -0600

    Add files via upload

---
+++ Practica03/Practica 3.ps1
@@ -0,0 +1,18 @@
+﻿# Consulta 1 a la API
+Invoke-RestMethod -Uri "<URL_API>/endpoint1"  # Reemplaza <URL_API> con la URL real
+
+# Consulta 2 a la API basada en la consulta previa
+$respuesta1 = Invoke-RestMethod -Uri "<URL_API>/endpoint1"
+$id = $respuesta1.id
+Invoke-RestMethod -Uri "<URL_API>/endpoint2/$id"
+
+# Consulta 3 a la API
+Invoke-RestMethod -Uri "<URL_API>/endpoint3"
+
+# Consulta 4 a la API basada en la consulta previa
+$respuesta3 = Invoke-RestMethod -Uri "<URL_API>/endpoint3"
+$parametro = $respuesta3.parametro
+Invoke-RestMethod -Uri "<URL_API>/endpoint4/$parametro"
+
+# Consulta 5 a la API
+Invoke-RestMethod -Uri "<URL_API>/endpoint5"
