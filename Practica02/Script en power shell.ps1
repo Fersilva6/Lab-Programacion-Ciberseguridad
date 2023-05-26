8b73d0a68e96687b30ca7863d8baa8dfe6c10878
Fersilva6 <134666529+Fersilva6@users.noreply.github.com>
2023-05-25 21:46:50 -0600

    Add files via upload

---
+++ Practica02/Script en power shell.ps1
@@ -0,0 +1,21 @@
+﻿# Definición de variable
+$nombre = "Juan"
+
+# Función con parámetro
+function Saludar($nombre) {
+  Write-Host "Hola, $nombre!"
+}
+
+# If
+if ($nombre -eq "Juan") {
+  # Ciclo
+  for ($i = 1; $i -le 5; $i++) {
+    Saludar $nombre
+  }
+}
+elseif ($nombre -eq "Pedro") {
+  Write-Host "Hola, Pedro!"
+}
+else {
+  Write-Host "El nombre no es Juan ni Pedro."
+}
