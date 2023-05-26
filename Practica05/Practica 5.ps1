afef3c4c55f1e581a682bcbb39c09d2ba06d3381
Fersilva6 <134666529+Fersilva6@users.noreply.github.com>
2023-05-25 22:11:51 -0600

    Add files via upload

---
+++ Practica05/Practica 5.ps1
@@ -0,0 +1,25 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+# URL del sitio web que deseas hacer scraping
+url = "https://www.ejemplo.com"
+
+# Realizar la solicitud HTTP GET a la página web
+response = requests.get(url)
+
+# Crear un objeto BeautifulSoup con el contenido HTML de la página web
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Extraer la información deseada del sitio web
+# Aquí se muestra un ejemplo para extraer los títulos de los elementos <h1> en la página
+titulos = soup.find_all("h1")
+
+# Crear un archivo para guardar la información extraída
+nombre_archivo = "datos_extraidos.txt"
+
+with open(nombre_archivo, "w") as archivo:
+    # Escribir los títulos en el archivo
+    for titulo in titulos:
+        archivo.write(titulo.text + "\n")
+
+print("Los datos han sido extraídos y guardados en el archivo:", nombre_archivo)
